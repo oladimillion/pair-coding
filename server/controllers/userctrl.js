37685eee1113207f29215e403eff8cdb6cbe8098
Akande Oladimeji <akandeoladimeji9@hotmail.com>
2017-09-19 14:56:17 +0100

    change react-router to react-router-dom

--- server/controllers/userctrl.js
+++ server/controllers/userctrl.js
@@ -54,127 +54,129 @@ function signup( req, res ) {
 
 	const { username, email, password, cpassword, phone } = req.body;
 
-	const errors = {};
+  const errors = {};
 
-	if ( username == null || username == "" ) {
-		errors.username = "username is required";
-	}
+  const re = /[\s]?[\w+\d+]{4,}/g
 
-	if ( email == null || email == "" ) {
-		errors.email = "email is required";
-	}
+  if ( username == null || username == "" ) {
+    errors.username = "username is required";
+  }
 
-	if ( phone == null || phone == "" ) {
-		errors.phone = "phone is required";
-	}
+  if ( email == null || email == "" ) {
+    errors.email = "email is required";
+  }
 
-	if ( password == null || password == "" ) {
-		errors.password = "password is required";
-	}
+  if ( phone == null || phone == "" ) {
+    errors.phone = "phone is required";
+  }
 
-	if ( password != cpassword ) {
-		errors.password = "Password must match";
-	}
+  if ( password == null || password == "" ) {
+    errors.password = "password is required";
+  }
 
-	if ( Object.keys( errors ).length != 0 ) {
-		return res.status( 400 ).json( {
-			success: false,
-			message: errors
-		} );
-	}
+  if ( password != cpassword ) {
+    errors.password = "Password must match";
+  }
 
-	const user = new User();
+  if ( Object.keys( errors ).length != 0 ) {
+    return res.status( 400 ).json( {
+      success: false,
+      message: errors
+    } );
+  }
 
-	user.username = username;
-	user.email = email.toLowerCase();
-	user.password = password;
-	user.phone = phone;
+  const user = new User();
 
-	user.save()
-		.then( function ( result ) {
-			// registration successful
+  user.username = username;
+  user.email = email.toLowerCase();
+  user.password = password;
+  user.phone = phone;
 
-			return res.status( 200 ).json( {
-				success: true,
-				message: "Registration Successful"
-			} );
+  user.save()
+    .then( function ( result ) {
+      // registration successful
 
-
-		} )
-		.catch( function ( err ) {
-
-			if ( err.errmsg ) {
-				if ( err.errmsg.indexOf( 'duplicate key error' ) !== -1 ) {
-
-					//unique error occured
-
-					const errors = {};
-
-					if ( err.errmsg.indexOf( 'username_1' ) !== -1 ) {
-						errors.username = "username already taken";
-					}
-
-					if ( err.errmsg.indexOf( 'email_1' ) !== -1 ) {
-						errors.email = "email already taken";
-					}
-
-					if ( err.errmsg.indexOf( 'phone_1' ) !== -1 ) {
-						errors.phone = "phone already taken";
-					}
-
-					if ( Object.keys( errors ) != 0 ) {
-						return res.status( 400 ).json( {
-							success: false,
-							message: errors
-						} );
-					} else {
-						throw err;
-					}
-
-				}
-			} else {
-
-				//validation error occured
-
-				const {
-					username,
-					password,
-					email,
-					phone
-				} = err.errors;
-
-				const errors = {};
-
-				if ( username != undefined ) {
-					errors.username = username.message;
-				}
-
-				if ( password != undefined ) {
-					errors.password = password.message;
-				}
-
-				if ( email != undefined ) {
-					errors.email = email.message;
-				}
-
-				if ( phone != undefined ) {
-					errors.phone = phone.message;
-				}
+      return res.status( 200 ).json( {
+        success: true,
+        message: "Registration Successful"
+      } );
 
 
-				if ( Object.keys( errors ) != 0 ) {
-					return res.status( 400 ).json( {
-						success: false,
-						message: errors
-					} );
-				} else {
-					throw err;
-				}
-			}
-		} );
+    } )
+    .catch( function ( err ) {
+
+      if ( err.errmsg ) {
+        if ( err.errmsg.indexOf( 'duplicate key error' ) !== -1 ) {
+
+          //unique error occured
+
+          const errors = {};
+
+          if ( err.errmsg.indexOf( 'username_1' ) !== -1 ) {
+            errors.username = "username already taken";
+          }
+
+          if ( err.errmsg.indexOf( 'email_1' ) !== -1 ) {
+            errors.email = "email already taken";
+          }
+
+          if ( err.errmsg.indexOf( 'phone_1' ) !== -1 ) {
+            errors.phone = "phone already taken";
+          }
+
+          if ( Object.keys( errors ) != 0 ) {
+            return res.status( 400 ).json( {
+              success: false,
+              message: errors
+            } );
+          } else {
+            throw err;
+          }
+
+        }
+      } else {
+
+        //validation error occured
+
+        const {
+          username,
+          password,
+          email,
+          phone
+        } = err.errors;
+
+        const errors = {};
+
+        if ( username != undefined ) {
+          errors.username = username.message;
+        }
+
+        if ( password != undefined ) {
+          errors.password = password.message;
+        }
+
+        if ( email != undefined ) {
+          errors.email = email.message;
+        }
+
+        if ( phone != undefined ) {
+          errors.phone = phone.message;
+        }
+
+
+        if ( Object.keys( errors ) != 0 ) {
+          return res.status( 400 ).json( {
+            success: false,
+            message: errors
+          } );
+        } else {
+          throw err;
+        }
+      }
+    } );
 }
 
 module.exports = {
-	signup,
-	signin
+  signup,
+  signin
 }
